43260016497fd45c0b2e81a3c8895127c9d4dff2
BardiaKV <132426459+BardiaKV@users.noreply.github.com>
2025-10-31 02:57:56 +0330

    complete parser file for step 2

--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,6 @@
-# parser.py
-
 from typing import List
 from .lexer import Token
-from .ast_nodes import ASTNode, NumberNode, BinaryOpNode
+from .ast_nodes import ASTNode, NumberNode, BinaryOpNode, FunctionNode, CallNode, IfNode, VariableNode
 
 class Parser:
     def __init__(self, tokens: List[Token]):
@@ -17,14 +15,34 @@ class Parser:
     def consume(self, expected_type: str = None) -> Token:
         token = self.peek()
         if expected_type and token.type != expected_type:
-            raise SyntaxError(f"Expected {expected_type}, got {token.type}")
+            raise SyntaxError(f"Expected {expected_type}, got {token.type} ({token.value})")
         self.pos += 1
         return token
 
     def parse(self) -> ASTNode:
-        return self.expr()
+        if self.peek().type == 'FUNC':
+            return self.parse_function()
+        else:
+            return self.expr()
 
-    # expr   : term ((PLUS | MINUS) term)*
+    def parse_function(self) -> FunctionNode:
+        self.consume('FUNC')
+        name = self.consume('IDENTIFIER').value
+        self.consume('LPAREN')
+        params = []
+        if self.peek().type != 'RPAREN':
+            while True:
+                param = self.consume('IDENTIFIER').value
+                params.append(param)
+                if self.peek().type == 'COMMA':
+                    self.consume('COMMA')
+                else:
+                    break
+        self.consume('RPAREN')
+        self.consume('ASSIGN')
+        body = self.expr()
+        return FunctionNode(name, params, body)
+
     def expr(self) -> ASTNode:
         node = self.term()
         while self.peek().type in ('PLUS', 'MINUS'):
@@ -33,7 +51,6 @@ class Parser:
             node = BinaryOpNode(node, op, right)
         return node
 
-    # term   : factor ((MUL | DIV) factor)*
     def term(self) -> ASTNode:
         node = self.factor()
         while self.peek().type in ('MUL', 'DIV'):
@@ -42,7 +59,6 @@ class Parser:
             node = BinaryOpNode(node, op, right)
         return node
 
-    # factor : NUMBER | LPAREN expr RPAREN
     def factor(self) -> ASTNode:
         token = self.peek()
         if token.type == 'NUMBER':
@@ -53,5 +69,33 @@ class Parser:
             node = self.expr()
             self.consume('RPAREN')
             return node
+        elif token.type == 'IF':
+            return self.parse_if()
+        elif token.type == 'IDENTIFIER':
+            name = token.value
+            self.consume('IDENTIFIER')
+            if self.peek().type == 'LPAREN':
+                self.consume('LPAREN')
+                args = []
+                if self.peek().type != 'RPAREN':
+                    while True:
+                        args.append(self.expr())
+                        if self.peek().type == 'COMMA':
+                            self.consume('COMMA')
+                        else:
+                            break
+                self.consume('RPAREN')
+                return CallNode(name, args)
+            else:
+                return VariableNode(name)
         else:
-            raise SyntaxError(f"Unexpected token: {token}")
+            raise SyntaxError(f"Unexpected token in factor: {token}")
+
+    def parse_if(self) -> IfNode:
+        self.consume('IF')
+        condition = self.expr()
+        self.consume('THEN')
+        then_branch = self.expr()
+        self.consume('ELSE')
+        else_branch = self.expr()
+        return IfNode(condition, then_branch, else_branch)
